a5ac63efe280b3f64923caf87f9b29933d5db8b9
psanker <psanker@splunk.com>
2015-06-03 21:53:17 -0700

    have correct SQL, not executing with golang

--- populateDb.go
+++ populateDb.go
@@ -26,7 +26,7 @@ if err != nil {
 defer db.Close()
 
 
-	content, err := ioutil.ReadFile("expenses4.csv")
+	content, err := ioutil.ReadFile("activities.csv")
 
 	lines := strings.Split(string(content), "\r")
 
@@ -44,26 +44,44 @@ defer db.Close()
 	for i, line := range lines {
 		if i > 1 {
 			splitStr := strings.Split(line, ",")
-			functionCode := splitStr[0]
-			if strings.Contains(functionCode, "Subtotal") {
-
+			var activityCode string
+			if strings.Contains(splitStr[0], "-") {
+				//this is an activity
+				activityCode = splitStr[0]
+				stmt1, _ := db.Prepare("INSERT INTO activities(code) VALUES(?)")
+				stmt2, _ := db.Prepare("INSERT INTO activities(name) VALUES(?)")
+				stmt1.Exec(splitStr[0])
+				stmt2.Exec(splitStr[1])
 			} else {
-				stmt, err := db.Prepare("INSERT INTO functions(code) VALUES(?)")
-				check(err)
-				stmt.Exec(functionCode)
+				//this is a sub activity
+				stmt1, _ := db.Prepare("INSERT INTO sub_activities(code) VALUES(?)")
+				stmt2, _ := db.Prepare("INSERT INTO sub_activities(name) VALUES(?)")
+				stmt1.Exec(splitStr[0])
+				stmt2.Exec(splitStr[1])
+				if activityCode != "" {
+					rows, _ := db.Query("SELECT id from activities where code = ?", activityCode)
+					var activityId int
+					for rows.Next() {
+						err := rows.Scan(&activityId)
+						check(err)
+						stmt3, err := db.Prepare("INSERT INTO sub_activities(activity_id) VALUES(?)")
+						stmt3.Exec(activityId)
+					}
+				}
+				rows, _ := db.Query("SELECT id from sub_activities where code= ?", splitStr[0])
+				var sub_activityId int
+				for rows.Next() {
+					err := rows.Scan(&sub_activityId)
+					check(err)
+					stmt5, _ := db.Prepare("INSERT INTO sub_activity_expenditure(district_id) VALUES(?)")
+					stmt6, _ := db.Prepare("INSERT INTO sub_activity_expenditure(sub_activity_id) VALUES(?)")
+					stmt7, _ := db.Prepare("INSERT INTO sub_activity_expenditure(expenditure) VALUES(?)")
+					stmt5.Exec(districtId)
+					stmt6.Exec(sub_activityId)
+					stmt7.Exec(splitStr[2])
+				}
 			}
-			stmt, err := db.Prepare("INSERT INTO functions(name) VALUES(?)")
-			expenseType := splitStr[1]
-			check(err)
-			stmt.Exec(expenseType)
-			check(err)
-			expenditure := splitStr[2]
-			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(district_id) VALUES(?)")
-			stmt.Exec(districtId)
-			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(expenditure) VALUES(?)")
-			stmt.Exec(expenditure)
-			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(subtype_id) VALUES(?)")
-			stmt.Exec(i-2)
+
 
 
 
