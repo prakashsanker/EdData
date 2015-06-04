83900e9ae620a0e6784e10fd056b2954b30e0c05
psanker <psanker@splunk.com>
2015-06-03 20:57:47 -0700

    script to add data to database

--- populateDb.go
+++ populateDb.go
@@ -5,8 +5,8 @@ import (
 	"database/sql"
     _ "github.com/go-sql-driver/mysql"
 	"log"
-	"os"
-	"encoding/csv"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
@@ -26,23 +26,48 @@ if err != nil {
 defer db.Close()
 
 
+	content, err := ioutil.ReadFile("expenses4.csv")
+
+	lines := strings.Split(string(content), "\r")
+
+	//only work so long as I have one district
+	rows, err := db.Query("SELECT id FROM districts")
+	var districtId int
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&districtId)
+		check(err)
+		log.Println(districtId)
+	}
+
+	for i, line := range lines {
+		if i > 1 {
+			splitStr := strings.Split(line, ",")
+			functionCode := splitStr[0]
+			if strings.Contains(functionCode, "Subtotal") {
+
+			} else {
+				stmt, err := db.Prepare("INSERT INTO functions(code) VALUES(?)")
+				check(err)
+				stmt.Exec(functionCode)
+			}
+			stmt, err := db.Prepare("INSERT INTO functions(name) VALUES(?)")
+			expenseType := splitStr[1]
+			check(err)
+			stmt.Exec(expenseType)
+			check(err)
+			expenditure := splitStr[2]
+			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(district_id) VALUES(?)")
+			stmt.Exec(districtId)
+			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(expenditure) VALUES(?)")
+			stmt.Exec(expenditure)
+			stmt, _ := db.Prepare("INSERT INTO subtype_expenditure(subtype_id) VALUES(?)")
+			stmt.Exec(i-2)
 
 
-	dat, err := os.Open("expenses.csv")
-	check(err)
 
-	defer dat.Close()
-
-	reader := csv.NewReader(dat)
-
-	reader.FieldsPerRecord = -1
-
-	rawCSVData, err := reader.ReadAll()
-
-	check(err)
-
-	for _, each := range rawCSVData {
-		fmt.Println(each)
+		}
 	}
 
 
