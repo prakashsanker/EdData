280aa74331f59420a5cafec94c93b8966a14a3b8
psanker <psanker@splunk.com>
2015-06-04 15:04:10 -0700

    got script to enter data working, but need to debug

--- populateDb.go
+++ populateDb.go
@@ -19,10 +19,6 @@ if err != nil {
 	log.Fatal("Error:", err.Error())
 }
 
-
-
-
-
 defer db.Close()
 
 
@@ -38,34 +34,37 @@ defer db.Close()
 	for rows.Next() {
 		err := rows.Scan(&districtId)
 		check(err)
-		log.Println(districtId)
 	}
 
+	rows, err = db.Query("SELECT DATABASE();")
+	var test string
+
+	for rows.Next() {
+		rows.Scan(&test)
+		fmt.Println(test)
+	}
+
+	var activityCode string
 	for i, line := range lines {
 		if i > 1 {
 			splitStr := strings.Split(line, ",")
-			var activityCode string
 			if strings.Contains(splitStr[0], "-") {
 				//this is an activity
 				activityCode = splitStr[0]
-				stmt1, _ := db.Prepare("INSERT INTO activities(code) VALUES(?)")
-				stmt2, _ := db.Prepare("INSERT INTO activities(name) VALUES(?)")
-				stmt1.Exec(splitStr[0])
-				stmt2.Exec(splitStr[1])
+				_, err := db.Exec("INSERT INTO activities(code, name) VALUES(?, ?)", splitStr[0], splitStr[1])
+				check(err)
 			} else {
 				//this is a sub activity
-				stmt1, _ := db.Prepare("INSERT INTO sub_activities(code) VALUES(?)")
-				stmt2, _ := db.Prepare("INSERT INTO sub_activities(name) VALUES(?)")
-				stmt1.Exec(splitStr[0])
-				stmt2.Exec(splitStr[1])
+				check(err)
 				if activityCode != "" {
 					rows, _ := db.Query("SELECT id from activities where code = ?", activityCode)
 					var activityId int
 					for rows.Next() {
-						err := rows.Scan(&activityId)
+						err = rows.Scan(&activityId)
 						check(err)
-						stmt3, err := db.Prepare("INSERT INTO sub_activities(activity_id) VALUES(?)")
-						stmt3.Exec(activityId)
+						_, err = db.Exec("INSERT INTO sub_activities(activity_id, code, name) VALUES(?,?,?)", activityId, splitStr[0], splitStr[1])
+						check(err)
+
 					}
 				}
 				rows, _ := db.Query("SELECT id from sub_activities where code= ?", splitStr[0])
@@ -73,12 +72,14 @@ defer db.Close()
 				for rows.Next() {
 					err := rows.Scan(&sub_activityId)
 					check(err)
-					stmt5, _ := db.Prepare("INSERT INTO sub_activity_expenditure(district_id) VALUES(?)")
-					stmt6, _ := db.Prepare("INSERT INTO sub_activity_expenditure(sub_activity_id) VALUES(?)")
-					stmt7, _ := db.Prepare("INSERT INTO sub_activity_expenditure(expenditure) VALUES(?)")
-					stmt5.Exec(districtId)
-					stmt6.Exec(sub_activityId)
-					stmt7.Exec(splitStr[2])
+					fmt.Println("EXPENDITURE")
+					fmt.Println(splitStr[len(splitStr) - 1])
+					fmt.Println(splitStr)
+					if splitStr[len(splitStr) - 1] != "N/A" {
+						_, err = db.Exec("INSERT INTO sub_activity_expenditure(district_id, sub_activity_id, expenditure) VALUES(?,?,?)", districtId, sub_activityId, splitStr[len(splitStr) - 1])
+						check(err)	
+					}
+
 				}
 			}
 		}
