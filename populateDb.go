8e8c1a000216410e6da766e1ec703dae8fee9bd5
psanker <psanker@splunk.com>
2015-06-05 11:24:08 -0700

    think I finally got cleaned data in for three tables

--- populateDb.go
+++ populateDb.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"io/ioutil"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -36,19 +37,13 @@ defer db.Close()
 		check(err)
 	}
 
-	rows, err = db.Query("SELECT DATABASE();")
-	var test string
-
-	for rows.Next() {
-		rows.Scan(&test)
-		fmt.Println(test)
-	}
 
 	var activityCode string
-	for i, line := range lines {
-		if i > 1 {
+	for _ , line := range lines {
+			line = strings.TrimSpace(line)
 			splitStr := strings.Split(line, ",")
-			if strings.Contains(splitStr[0], "-") {
+			strings.Replace(splitStr[1], "\"", "", -1)
+			if isUpper(splitStr[1]) && !strings.Contains("Subtotal", splitStr[2]){
 				//this is an activity
 				activityCode = splitStr[0]
 				_, err := db.Exec("INSERT INTO activities(code, name) VALUES(?, ?)", splitStr[0], splitStr[1])
@@ -56,38 +51,60 @@ defer db.Close()
 			} else {
 				//this is a sub activity
 				check(err)
-				if activityCode != "" {
-					rows, _ := db.Query("SELECT id from activities where code = ?", activityCode)
-					var activityId int
-					for rows.Next() {
-						err = rows.Scan(&activityId)
-						check(err)
-						_, err = db.Exec("INSERT INTO sub_activities(activity_id, code, name) VALUES(?,?,?)", activityId, splitStr[0], splitStr[1])
-						check(err)
-
+				if !strings.Contains(splitStr[0], "Subtotal") && !strings.Contains(splitStr[0], "Total Expenditures") {
+					if activityCode != "" {
+						rows, _ := db.Query("SELECT id from activities where code = ?", activityCode)
+						var activityId int
+						for rows.Next() {
+							err = rows.Scan(&activityId)
+							check(err)
+							_, err = db.Exec("INSERT INTO sub_activities(activity_id, code, name) VALUES(?,?,?)", activityId, splitStr[0], splitStr[1])
+							check(err)
+						}
 					}
 				}
+
 				rows, _ := db.Query("SELECT id from sub_activities where code= ?", splitStr[0])
 				var sub_activityId int
 				for rows.Next() {
 					err := rows.Scan(&sub_activityId)
 					check(err)
-					fmt.Println("EXPENDITURE")
-					fmt.Println(splitStr[len(splitStr) - 1])
-					fmt.Println(splitStr)
 					if splitStr[len(splitStr) - 1] != "N/A" {
 						_, err = db.Exec("INSERT INTO sub_activity_expenditure(district_id, sub_activity_id, expenditure) VALUES(?,?,?)", districtId, sub_activityId, splitStr[len(splitStr) - 1])
 						check(err)	
 					}
-
 				}
 			}
-		}
 	}
 
+	// content, err = ioutil.ReadFile("expenditureTypes.csv")
+	// lines := strings.Split(string(content), "\r")
+
+	// for i, line := range lines {
+	// 	if i > 1 {
+	// 		splitStr := strings.Split(line, ",")
+	// 		if splitStr[2] != "" {
+	// 			_, err := db.Exec("INSERT INTO expenditure_types(name, code) VALUES(?, ?) ", splitStr[2], splitStr[3])
+	// 			check(err)
+	// 		}
+
+	// 		rows, _ := db.Query("SELECT id from sub_activities WHERE activity_id=?", )
+	// 	}
+	// }
 
 
 
+}
+
+func isUpper(str string) bool {
+	for _, char := range str {
+		if char != '-' && char != ' ' && char != '"' {
+			if (!unicode.IsUpper(char)) {
+				return false
+			}	
+		}
+	}
+	return true
 }
 
 func check(e error) {
