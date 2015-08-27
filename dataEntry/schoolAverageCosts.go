3d8edb96beb53008082f1c2e19498c0f1f6fb1b6
psanker <psanker@splunk.com>
2015-08-26 21:46:59 -0700

    realized that average costs are only for districts, trying to take this into account by adding districts table, adding district_schools_mapping table, district_cost table

--- dataEntry/schoolAverageCosts.go
+++ dataEntry/schoolAverageCosts.go
@@ -15,40 +15,58 @@ func main() {
 	db.SetMaxOpenConns(0)
 
 	check(err)
+	fmt.Println("WHAT")
 
 	if err != nil {
+		fmt.Println("HELLO")
 		fmt.Println("Failed to prepare connection to database")
 		log.Fatal("Error: ", err.Error())
+	}
 
 		defer db.Close()
 
 		content, err := ioutil.ReadFile("currentExpense1314.csv")
+		check(err)
 
 		lines := strings.Split(string(content), "\r")
 
-		rows, err := db.Query("CREATE TABLE IF NOT EXISTS school_expenses(id INT(11) NOT NULL AUTO_INCREMENT, school_id INT(11) NOT NULL, expenditure INT(25), current_expense_ada INT(25), current_expense_per_ada INT(25), PRIMARY KEY(id), FOREIGN KEY(school_id) REFERENCES schools(id))")
+		rows, err := db.Query("CREATE TABLE IF NOT EXISTS district_expenses(id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, expenditure INT(25), current_expense_ada INT(25), current_expense_per_ada INT(25), PRIMARY KEY(id), FOREIGN KEY(school_id) REFERENCES districts(id))")
 		check(err)
+		stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS districts (id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, PRIMARY KEY (id));")
+		check(err)
+		stmt.Exec()
 		defer rows.Close()
 
 		for i, row := range(lines) {
 			if i > 0 {
 				line := strings.TrimSpace(row)
 				splitStr := strings.Split(line, ",")
-				schoolName := splitStr[3]
-				schoolRow, err := db.Query("SELECT id from schools where name=?", schoolName)
+				schoolName := splitStr[2]
+				test := "SELECT id from schools where name LIKE \"%" + schoolName + "%\""
+				fmt.Println(line)
+				fmt.Println(splitStr)
+				schoolRow, err := db.Query(test)
 				check(err)
 				hasNextRow := schoolRow.Next()
 				if hasNextRow {
 					//is a valid school
 					var id int
 					err = schoolRow.Scan(&id)
-					_, err = db.Exec("INSERT INTO school_expenses(school_id, expenditure, current_expense_ada, current_expense_per_ada) VALUES(?,?,?,?)", id, splitStr[4], splitStr[5], splitStr[6])
+					length := len(splitStr)
+					startingIndex := length - 4
+					expenditure := strings.Replace(splitStr[startingIndex], " \" ", "", -1)
+					currentExpenseAda := strings.Replace(splitStr[startingIndex + 1], "\"", "", -1)
+					currentExpensePerAda := strings.Replace(splitStr[startingIndex + 2], "\"", "", -1)
+					expenditure = strings.Replace(expenditure, "\"", "", -1)
+					_, err = db.Exec("INSERT INTO district_expenses(school_id, expenditure, current_expense_ada, current_expense_per_ada) VALUES(?,?,?,?)", id, expenditure, currentExpenseAda, currentExpensePerAda)
 					check(err)
+				} else {
+					fmt.Print("no school")
+					fmt.Println("schoolName : " + schoolName)
 				}
 				schoolRow.Close()
 			}
 		}
-	}
 }
 
 func check(e error) {
