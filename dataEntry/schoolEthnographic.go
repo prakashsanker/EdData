bb18ede5cb8110467d07bc2d33be7e02b7c88545
psanker <psanker@splunk.com>
2015-08-31 19:18:28 -0700

    populate districts, populate districts_schools_mapping, but have duplicates

--- dataEntry/schoolEthnographic.go
+++ dataEntry/schoolEthnographic.go
@@ -31,10 +31,10 @@ func main() {
 	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS demographics (id INT(11) NOT NULL AUTO_INCREMENT, school_id INT(11) NOT NULL, ethnicity ENUM('1','2','3','4','5','6','7','8','9'), gender ENUM('F', 'M'), kindergarten INT(11), grade_1 INT(11), grade_2 INT(11), grade_3 INT(11), grade_4 INT(11), grade_5 INT(11), grade_6 INT(11), grade_7 INT(11), grade_8 INT(11), grade_9 INT(11), grade_10 INT(11), grade_11 INT(11), grade_12 INT(11), ungr_elem INT(11), ungr_sec INT(11), total INT(11), adult INT(11), FOREIGN KEY(school_id) REFERENCES schools(id), PRIMARY KEY(id))")
 	check(err)
 	stmt.Exec()
-	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS districts (id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, PRIMARY KEY (id));")
+	stmt, err = db.Prepare("CREATE TABLE IF NOT EXISTS districts (id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, PRIMARY KEY (id));")
 	check(err)
 	stmt.Exec()
-	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS districts_schools_mapping (id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, school_id INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY(school_id) REFERENCES schools(id));")
+	stmt, err = db.Prepare("CREATE TABLE IF NOT EXISTS districts_schools_mapping (id INT(11) NOT NULL AUTO_INCREMENT, district_id INT(11) NOT NULL, school_id INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY(school_id) REFERENCES schools(id));")
 	check(err)
 	stmt.Exec()
 	defer rows.Close()
@@ -46,25 +46,53 @@ func main() {
 			if splitStr[3] != "" {
 				schoolRows, err := db.Query("SELECT id from schools where name=?", splitStr[3])
 				check(err)
+				// var schoolId sql.Result
 				hasNextRow := schoolRows.Next()
 				if !hasNextRow {
-					db.Exec("INSERT INTO schools(name) VALUES (?)", splitStr[3])
+					_, err = db.Exec("INSERT INTO schools(name) VALUES (?)", splitStr[3])
+					check(err)
 				} else {
 					//has a school row
 					var id int
 					err = schoolRows.Scan(&id)
 					check(err)
+					schoolRows.Close()
 					splitStrLen := len(splitStr)
 					startingIndex := splitStrLen - 19
 					demographicRow, err := db.Query("SELECT id from demographics where id=?", id)
 					hasNextRow = demographicRow.Next()
+					demographicRow.Close()
 					if !hasNextRow {
-						_, err = db.Exec("INSERT INTO demographics(school_id, ethnicity, gender, kindergarten, grade_1, grade_2, grade_3, grade_4, grade_5, grade_6, grade_7, grade_8, grade_9, grade_10, grade_11, grade_12, ungr_elem, ungr_sec, total, adult) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, ?, ?)", id, splitStr[startingIndex], splitStr[startingIndex + 1], splitStr[startingIndex + 2],splitStr[startingIndex + 3], splitStr[startingIndex + 4], splitStr[startingIndex + 5], splitStr[startingIndex + 6], splitStr[startingIndex + 7],splitStr[startingIndex + 8], splitStr[startingIndex + 9], splitStr[startingIndex + 10], splitStr[startingIndex + 11], splitStr[startingIndex + 12], splitStr[startingIndex + 13], splitStr[startingIndex + 14], splitStr[startingIndex + 15], splitStr[startingIndex + 16], splitStr[startingIndex + 17], splitStr[startingIndex + 18])
+						_, err := db.Exec("INSERT INTO demographics(school_id, ethnicity, gender, kindergarten, grade_1, grade_2, grade_3, grade_4, grade_5, grade_6, grade_7, grade_8, grade_9, grade_10, grade_11, grade_12, ungr_elem, ungr_sec, total, adult) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, ?, ?)", id, splitStr[startingIndex], splitStr[startingIndex + 1], splitStr[startingIndex + 2],splitStr[startingIndex + 3], splitStr[startingIndex + 4], splitStr[startingIndex + 5], splitStr[startingIndex + 6], splitStr[startingIndex + 7],splitStr[startingIndex + 8], splitStr[startingIndex + 9], splitStr[startingIndex + 10], splitStr[startingIndex + 11], splitStr[startingIndex + 12], splitStr[startingIndex + 13], splitStr[startingIndex + 14], splitStr[startingIndex + 15], splitStr[startingIndex + 16], splitStr[startingIndex + 17], splitStr[startingIndex + 18])
+						check(err)
 					}
-					stmt, err := db.Prepare("INSERT INTO districts(name) VALUES(?)", splitStr[2])
+					districtRow, err := db.Query("SELECT id from districts where name=?", splitStr[2]);
 					check(err)
+					fmt.Println(districtRow)
+					districtRow.Close()
+					hasNextRow = districtRow.Next()
+					if !hasNextRow {
+						districtResult, err := db.Exec("INSERT INTO districts(name) VALUES(?)", splitStr[2])
+						check(err)
+						districtId, err := districtResult.LastInsertId()
+						check(err)
+						districtSchoolRow, err := db.Query("SELECT id from districts_schools_mapping WHERE district_id=? AND school_id=?", districtId, id)
+						hasNextRow = districtSchoolRow.Next()
+						districtSchoolRow.Close()
+						if !hasNextRow {
+							fmt.Println(districtId)
+							fmt.Println(id)
+							_, err = db.Exec("INSERT INTO districts_schools_mapping(district_id, school_id) VALUES(?,?)", districtId, id)
+							check(err)
+						}
+						
+					}
+
+
+
 				}
 				schoolRows.Close()
+
 			}
 		}
 	}
